f00b7451c8ed2025ecefa06b376a88af8b91d210
alexewu <alexewu@umich.edu>
2019-08-19 15:46:40 -0400

    navbar active page implementation

--- components/navbar.component.js
+++ components/navbar.component.js
@@ -4,12 +4,37 @@
 
     app.component('navbar', {
         templateUrl: 'components/navbar.html',
-        controller: 'navbarController'
+        controller: 'navbarController',
+        bindings: {
+            active: "@"
+        }
     });
 
     app.controller('navbarController', navbarController);
 
     function navbarController() {
         var ctrl = this;
+        ctrl.active = "aboutMe"
+
+        ctrl.$init = function () {
+            ctrl.aboutMe = "inactive";
+            ctrl.contact = "inactive";
+            ctrl.hobbies = "inactive";
+            ctrl.projects = "inactive";
+            switch(ctrl.active) {
+                case "aboutMe":
+                    ctrl.aboutMe = "active";
+                case "contact":
+                    ctrl.contact = "active";
+                case "hobbies":
+                    ctrl.hobbies = "active";
+                case "projects":
+                    ctrl.projects = "active";
+                default:
+                    break;
+            }
+        }
+        
+
     }
 })();
