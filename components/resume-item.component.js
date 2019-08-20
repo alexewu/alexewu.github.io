041ea14a9202b0245d1bd15164674c59fd7dac0e
alexewu <alexewu@umich.edu>
2019-08-20 13:29:21 -0400

    resume item component

---
+++ components/resume-item.component.js
@@ -0,0 +1,16 @@
+(function() {
+
+    var app = angular.module("personalWebsite");
+
+    app.component('resumeItem', {
+        templateUrl: 'components/resume-item.html',
+        controller: 'resumeItemController'
+    });
+
+    app.controller('resumeItemController', resumeItemController);
+
+    function resumeItemController() {
+        var ctrl = this;
+        
+    }
+})();
