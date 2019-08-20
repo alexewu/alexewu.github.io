ba32e25eac7d0077daa966a763ad1ee81e8e2284
alexewu <alexewu@umich.edu>
2019-08-20 15:02:48 -0400

    resume item styling

--- components/resume-section.component.js
+++ components/resume-section.component.js
@@ -3,14 +3,14 @@
     var app = angular.module("personalWebsite");
 
     app.component('resumeSection', {
-        templateUrl: 'components/contact-form.html',
-        controller: 'contactFormController'
+        templateUrl: 'components/resume-section.html',
+        controller: 'resumeSectionController'
     });
 
-    app.controller('contactFormController', contactFormController);
-    tableController.$inject = ["$http"];
+    app.controller('resumeSectionController', resumeSectionController);
+    resumeSectionController.$inject = ["$http"];
 
-    function contactFormController($http) {
+    function resumeSectionController($http) {
         var ctrl = this;
 
         $http.get('projects.json').then(function(response) {
