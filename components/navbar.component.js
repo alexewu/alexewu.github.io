b6e97f60aa0b3ddb88032790130e84af1029d326
alexewu <alexewu@umich.edu>
2019-08-21 10:46:31 -0400

    border around active page

--- components/navbar.component.js
+++ components/navbar.component.js
@@ -15,24 +15,22 @@
     function navbarController() {
         //TODO: put border around active tab
         var ctrl = this;
-
-        ctrl.$init = function () {
-            ctrl.aboutMe = "inactive";
-            ctrl.contact = "inactive";
-            ctrl.hobbies = "inactive";
-            ctrl.projects = "inactive";
-            switch(ctrl.active) {
-                case "aboutMe":
-                    ctrl.aboutMe = "active";
-                case "contact":
-                    ctrl.contact = "active";
-                case "hobbies":
-                    ctrl.hobbies = "active";
-                case "projects":
-                    ctrl.projects = "active";
-                default:
-                    break;
-            }
+        
+        ctrl.aboutMe = "inactive";
+        ctrl.contact = "inactive";
+        ctrl.hobbies = "inactive";
+        ctrl.projects = "inactive";
+        switch(ctrl.active) {
+            case "aboutMe":
+                ctrl.aboutMe = "active";
+            case "contact":
+                ctrl.contact = "active";
+            case "hobbies":
+                ctrl.hobbies = "active";
+            case "projects":
+                ctrl.projects = "active";
+            default:
+                break;
         }
         
 
