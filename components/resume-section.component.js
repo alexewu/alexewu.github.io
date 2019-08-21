267c20ad0b22a2ebe24faa4fef0a8c924f088487
alexewu <alexewu@umich.edu>
2019-08-20 22:27:03 -0400

    making other sections show up

--- components/resume-section.component.js
+++ components/resume-section.component.js
@@ -22,12 +22,15 @@
             {
                 case "experience":
                     ctrl.jsonFileName = 'experience.json';
+                    ctrl.sectionName = 'Experience';
                     break;
                 case "education":
                     ctrl.jsonFileName = 'education.json';
+                    ctrl.sectionName = 'Education';
                     break;
                 case "projects":
                     ctrl.jsonFileName = 'projects.json';
+                    ctrl.sectionName = 'Projects';
                     break;
                 default:
                     break;
