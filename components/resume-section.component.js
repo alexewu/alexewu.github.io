6e58aed21d86965b874aeb7a80b1fd7f1f32507c
alexewu <alexewu@umich.edu>
2019-08-22 15:55:09 -0400

    refactoring resume section to use one json file

--- components/resume-section.component.js
+++ components/resume-section.component.js
@@ -17,40 +17,30 @@
         var ctrl = this;
         
         $http.get('resume.json')
-                .then(function(response) {
-                    ctrl.resume = response.data;
-                });
-
-        ctrl.$onInit = function () {
-            ctrl.jsonFileName = "";
-            switch(ctrl.section)
-            {
-                case "education":
-                    ctrl.items = ctrl.resume.education;
-                    ctrl.sectionName = 'Education';
-                    ctrl.resume_item_style = "resume_item_1";
-                    ctrl.resume_header_style = "resume_header_1";
-                    break;
-                case "experience":
-                    ctrl.items = ctrl.resume.experience;
-                    ctrl.sectionName = 'Experience';
-                    ctrl.resume_item_style = "resume_item_2";
-                    ctrl.resume_header_style = "resume_header_2";
-                    break;
-                case "projects":
-                    ctrl.items = ctrl.resume.projects;
-                    ctrl.sectionName = 'Projects';
-                    ctrl.resume_item_style = "resume_item_3";
-                    ctrl.resume_header_style = "resume_header_3";
-                    break;
-                default:
-                    break;
-            }
-            
-            
-        }
-       
-
-        
+            .then(function(response) {
+                ctrl.resume = response.data;
+                switch(ctrl.section) {
+                    case "education":
+                        ctrl.items = ctrl.resume.education;
+                        ctrl.sectionName = 'Education';
+                        ctrl.resume_item_style = "resume_item_1";
+                        ctrl.resume_header_style = "resume_header_1";
+                        break;
+                    case "experience":
+                        ctrl.items = ctrl.resume.experience;
+                        ctrl.sectionName = 'Experience';
+                        ctrl.resume_item_style = "resume_item_2";
+                        ctrl.resume_header_style = "resume_header_2";
+                        break;
+                    case "projects":
+                        ctrl.items = ctrl.resume.projects;
+                        ctrl.sectionName = 'Projects';
+                        ctrl.resume_item_style = "resume_item_3";
+                        ctrl.resume_header_style = "resume_header_3";
+                        break;
+                    default:
+                        break;
+                }
+            });
     }
 })();
