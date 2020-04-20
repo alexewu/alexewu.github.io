d65553ef6b6df1029c952a93f01ae23254506ce3
Alexandra Wu <alexandrawu@Alexandras-MBP.attlocal.net>
2020-04-20 02:04:35 -0400

    anchor for navbar

--- components/navbar.component.js
+++ components/navbar.component.js
@@ -11,8 +11,9 @@
     });
 
     app.controller('navbarController', navbarController);
+    navbarController.$inject = ["$anchorScroll", "$location"];
 
-    function navbarController() {
+    function navbarController($anchorScroll, $location) {
         //TODO: put border around active tab
         var ctrl = this;
         
@@ -24,23 +25,41 @@
         switch(ctrl.active) {
             case "aboutMe":
                 ctrl.aboutMe = "active";
+                ctrl.showAboutMeNavBar = true;
                 break;
             case "contact":
                 ctrl.contact = "active";
+                ctrl.showAboutMeNavBar = false;
                 break;
             case "resume":
                 ctrl.resume = "active";
+                ctrl.showAboutMeNavBar = false;
                 break;
             case "hobbies":
                 ctrl.hobbies = "active";
+                ctrl.showAboutMeNavBar = false;
                 break;
             case "projects":
                 ctrl.projects = "active";
+                ctrl.showAboutMeNavBar = false;
                 break;
             default:
                 break;
         }
         
+        ctrl.gotoPhotography = function() {
+            $location.hash('photography');
+            $anchorScroll();
+        };
 
+        ctrl.gotoBujo = function() {
+            $location.hash('bujo');
+            $anchorScroll();
+        };
+
+        ctrl.gotoPiggies = function() {
+            $location.hash('piggies');
+            $anchorScroll();
+        };
     }
 })();
